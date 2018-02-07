bb5bf9711fda76126d86bb05470add96981b11be
tareq89 <tareqaziz.aust.cse@gmail.com>
2018-02-07 14:29:57 +0600

    only hr can input attendance

--- server/routes/HRroutes.js
+++ server/routes/HRroutes.js
@@ -6,10 +6,18 @@ const createAttendance = require('../middlewares/createAttendance');
 router.use(checkJWT);
 
 router.post('/input', (req, res) => {
-    createAttendance(req.body, (err, result) => {
-        if (err) res.sendStatus(500);
-        else res.sendStatus(200);
-    });
+    console.log(req.decoded.role.indexOf('hr') > 0)
+    if(req.decoded.role.indexOf('hr') > 0) {
+        createAttendance(req.body, (err, result) => {
+            if (err) res.sendStatus(500);
+            else res.sendStatus(200);
+        });
+    } else {
+        res.json({
+            error: 'only hr can input attendance'
+        });
+    }
+    
 });
 
 
