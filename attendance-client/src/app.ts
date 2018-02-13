96de89275074e730438373e8debcfce235973967
DESKTOP-3T50VJ5\Exam <uddin1.zashim@gmail.com>
2018-02-13 14:54:22 +0600

    trying to post

--- attendance-client/src/app.ts
+++ attendance-client/src/app.ts
@@ -2,32 +2,21 @@ import '../main.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const loginButton: any = <HTMLButtonElement>document.getElementById('loginButton');
-
-
-const noticeTemplate: string = `<li class="list-group-item">
-                    <div>
-                        <h1>Tomorrow Office closed</h1>
-                        <h6>HR notice</h6>
-                        <span>Tomorrow is office closed due to some reason</span>
-                    </div>
-                </li>`;
-
 function createNoticeTemplate (notice: any): any {
     const title: any = notice.title;
-    const subTitle: any = notice.subTitle;
+    const subTitle: any = notice.subtitle;
     const description: any = notice.description;
 
     const list: any = document.createElement('li');
     const div : any = document.createElement('div');
-    const h1: any = document.createElement('h1');
+    const h3: any = document.createElement('h1');
     const h6: any = document.createElement('h6');
     const span: any = document.createElement('span');
 
-    h1.appendChild(document.createTextNode(title));
+    h3.appendChild(document.createTextNode(title));
     h6.appendChild(document.createTextNode(subTitle));
-    span.appendChild(document.createTextNode(description));
-    div.appendChild(h1);
+    span.innerHTML = description;
+    div.appendChild(h3);
     div.appendChild(h6);
     div.appendChild(span);
 
@@ -36,24 +25,12 @@ function createNoticeTemplate (notice: any): any {
     return list;
 }
 
-
-
-loginButton.addEventListener('click', () => {
-    console.log("ASdSADA");
-    // e.preventDefault();
-});
-// console.log(loginButton);
-
-
-
-const noticeUl = <HTMLUListElement>document.getElementById('notice-ul');
-fetch('http://localhost:3000/notice/get')
+function loadNotices() {
+    const noticeUl = <HTMLUListElement>document.getElementById('notice-ul');
+    fetch('http://localhost:3000/notice/get')
     .then((response: any) => {
-        response.json().then(function(notices: any) {
-            console.log("this is the notices")
-            console.log(notices);
-            for(let notice of notices) {
-                console.log(notice);
+        response.json().then(function(notices: any) {            
+            for(let notice of notices) {            
                 const list = createNoticeTemplate(notice);
                 noticeUl.appendChild(list);
             }
@@ -63,3 +40,51 @@ fetch('http://localhost:3000/notice/get')
     .catch((error) => {
         alert(error);
     })
+}
+
+function postNotice() {
+    class Notice {
+        title: string;
+        subtitle: string;
+        description: string;
+        createtime: number;
+        constructor() {
+            this.title = (<HTMLInputElement>document.getElementById('title')).value;
+            this.subtitle = (<HTMLInputElement>document.getElementById('subtitle')).value;
+            this.description = (<HTMLInputElement>document.getElementById('notice')).value;
+            this.createtime = Math.floor(Date.now() / 1000);
+        }        
+    };
+
+    const notice = JSON.stringify(new Notice());
+    console.log(notice)
+    // fetch('http://localhost:3000/notice/post', {
+    //     method: 'post',
+    //     body: notice  
+    // }).then(function(result) { 
+    //     console.log(result);
+    //     loadNotices(); 
+    // });
+
+    fetch('http://localhost:3000/notice/post', {
+                method: 'POST',
+                headers : new Headers(),
+                body: notice
+            }).then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                loadNotices();
+            })
+            .catch((err)=>console.log(err))
+}
+
+
+
+
+const loginButton: any = document.getElementById('loginButton');
+loginButton.addEventListener('click', (e: Event) => {
+    postNotice();
+    e.preventDefault();
+});
+
+loadNotices();
--- server/routes/NoticeRoutes.js
+++ server/routes/NoticeRoutes.js
@@ -12,19 +12,19 @@ router.get('/get', (req, res) => {
         {
             title: "Title 1",
             subtitle: "Subtitle 1",
-            description: "description description description description description description",
+            description: "description description description description description description <b>This is bold</b>",
             createTime: 298783927893
         },
         {
             title: "Title 1",
             subtitle: "Subtitle 1",
-            description: "description description description description description description",
+            description: "description <b>This is bold</b> description description description description",
             createTime: 298783927893
         },
         {
             title: "Title 1",
             subtitle: "Subtitle 1",
-            description: "description description description description description description",
+            description: "<b>This is bold</b> description description description description description description",
             createTime: 298783927893
         }
     ]
@@ -34,7 +34,8 @@ router.get('/get', (req, res) => {
 
 
 router.post('/post', (req, res) => {
-
+    console.log(req.body);
+    res.sendStatus(200);
 });
 
 
